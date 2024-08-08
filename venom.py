b90f0f10aa59a6a618f28df21cc55987950ae2d7
GamerYTK <128370702+KamalkumarKk@users.noreply.github.com>
2024-08-08 22:21:20 +0530

    Update venom.py
    Signed-off-by: GamerYTK <128370702+KamalkumarKk@users.noreply.github.com>

--- venom.py
+++ venom.py
@@ -10,7 +10,7 @@ MAX_RESTARTS = 5
 RESTART_PERIOD = 300  # Seconds
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-bot = Bot()
+bot = Bot(API_TOKEN)
 
 def start_bot():
     """Start the bot script as a subprocess."""
